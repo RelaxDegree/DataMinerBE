6dff4ec89dc89e32c03fbc43864866e816bd1bb3
RelaxDegree <1043511190@qq.com>
2022-12-16 16:27:59 +0800

    后端整合

---
+++ DataMining/main.py
@@ -0,0 +1,60 @@
+import util_m
+from DataMining import apriori
+from DataMining import fp_growth
+from DataMining.TheRomanceOfThreeKingdomsDataProcess import TheRomanceOfThreeKingdomsDataPreprocessing
+from DataMining.theNationalCongressDataPreprocessing import TheNationalCongressDataPreprocessing
+from sys import stdin
+from DataMining.countWordFrequency import CountWordFrequency
+from DataMining.insuranceDataProcessing import insurenceDataProcessing
+from DataMining.patientDataProcessing import preprocessPatientDataHarshly, preprocessPatientDataMoreDetailedly
+from DataMining.patternAssess import PatternAssess
+import pandas as pd
+from DataMining.shoppingBasketDataProcess import preprocessShoppingtData2
+
+def facade(ftype, support, confidence, fileName):
+    # data = patientDataPreprocessing()
+    theNationalCongressDataPreprocessing = TheNationalCongressDataPreprocessing()
+    threeKingdomsDataProcess = TheRomanceOfThreeKingdomsDataPreprocessing()
+    cwf = CountWordFrequency()
+    print("请输入\n"
+          "选择频繁模式挖掘算法（1：Apriori  2：fp-growth）\n"
+          "支持度 置信度(0,1)\n"
+          "以空格分隔 如：“2 0.05 0.05 "
+          "输入#退出")
+
+    # data = preprocessPatientDataHarshly()
+    # data = preprocessPatientDataMoreDetailedly()
+    # data = insurenceDataProcessing()
+    # data = threeKingdomsDataProcess.dataPreprocessing()
+    data = theNationalCongressDataPreprocessing.dataPreprocessing()
+    # data = preprocessShoppingtData2()
+    # print('这里时预处理后的数据', data)
+    if ftype == "Apriori":
+        apr = apriori.Apriori(datas=data, support=support, confidence=confidence)
+        # print(apr.freq_set)
+        print(util_m.relate_rules2str(apr.rel_rules))
+        pa = PatternAssess(data, apr.rel_rules)
+        pa.calculateLiftAndCosine()
+        pa.calculateAllConfidenced()
+        pa.calculateKulczynski()
+    else:
+        # print(data)
+        fp = fp_growth.FPGrowth(datas=data, support=support, confidence=confidence)
+        # print("频繁模式树为：")
+        # fp.fp_tree_root_point.print()
+        print("频繁项集", fp.freq_set)
+        # print("未格式化的关联规则",fp.relate_rule)
+        print(util_m.relate_rules2str(fp.relate_rule))
+        print("下面是模式评估相关量：**********************************************************************************")
+        pa = PatternAssess(data, fp.relate_rule)
+        pa.calculateLiftAndCosine()
+        pa.calculateAllConfidenced()
+        pa.calculateKulczynski()
+        print()
+
+'''
+对于病人数据集结果不错的参数有 : 2 0.3 0.7 ;
+购物篮1: 2 0.02 0.025
+购物篮2 groceries: 
+
+'''
--- app01/views.py
+++ app01/views.py
@@ -1,11 +1,37 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-
-
+import json
+from DataMining.main import facade
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 
 
-# @csrf_exempt
+def respondDataToFront(preData):
+    data = {
+        'code': 200,
+        'message': "获取成功",
+        'data': preData
+    }
+    print("完成发送任务")
+    return JsonResponse(data=data, safe=False)
+
+
+@csrf_exempt
 def process(request):
-    return HttpResponse("welcome")
+    # data = json.loads(request.body)
+    # print(type(data))
+    fileName = request.GET.get("fileName")
+    support = request.GET.get("support")
+    confident = request.GET.get("confident")
+    alg = request.GET.get("alg")
+    print(fileName, float(support), float(confident), alg)
+    preData = {}
+    lst = ['11', '22', '33', '44']
+
+    preData['node'] = 'welcome'
+    preData['lst'] = lst
+    facade(alg, float(support), float(confident), fileName)
+    return HttpResponse(respondDataToFront(preData))
+
+# def next
